0218de56e577ad49464a931b2ea2e6ed807e53b9
dm89f11 <dm89f11@gmail.com>
2023-02-21 16:07:43 +0530

    bottom nav done

--- components/utils/CheckoutCartNavBtn.js
+++ components/utils/CheckoutCartNavBtn.js
@@ -7,8 +7,15 @@ function CheckoutCartNavBtn() {
   let totalItems = orderItems.totalItems;
 
   return (
-    <div style={{height:'80px'}} className='flex items-center z-50 absolute w-screen bg-dominos-green' >
-      Checkout
+    <div style={{height:'80px'}} 
+      className='flex items-center z-50 absolute w-screen bg-dominos-green' 
+    >
+        <button
+        // onClick={toggleBottomcart} 
+        className='w-screen my-2 text-xl border-0 outline-none py-5 text-white rounded-sm md:w-64 md:border-2 md:py-2 md:px-1 md:ml-auto md:mr-3 '
+        >
+          Checkout ( { totalItems } ) : ₹ {" "} {totalPrice.toFixed(2)}
+      </button>
     </div>
   )
 }
